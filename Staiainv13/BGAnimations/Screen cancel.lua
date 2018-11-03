e92453996210e14e4d9316caecec55ece6a61a7a
Brendon <29536109+11brendon@users.noreply.github.com>
2018-11-03 15:52:02 -0400

    Update Screen cancel.lua

--- Staiainv13/BGAnimations/Screen cancel.lua
+++ Staiainv13/BGAnimations/Screen cancel.lua
@@ -9,15 +9,9 @@ local t =
                 self:stretchto(SCREEN_LEFT,SCREEN_TOP,SCREEN_RIGHT,SCREEN_BOTTOM):cropleft(1):fadeleft(0.5):linear(0.5):cropleft(-0.5):diffuse(color("#000000"))
             end
         },
-        Def.Sound {
-            InitCommand = function(self)
-                self:load("\Themes\Staiainv13\BGAnimations\Screen cancel.ogg")
-            end,
-            StartTransitioningCommand = function(self)
-                self:play()
-            end
-        }
-        
-    }
+        LoadActor ( "Screen cancel.ogg" )..{StartTransitioningCommand=function(self)
+			self:play()
+		end}
+	}
 
 return t
