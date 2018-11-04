42ef36f1dacf7cb1bed30b787640b512f849e3bc
Brendon <29536109+11brendon@users.noreply.github.com>
2018-11-04 08:11:24 -0500

    Update ScreenTitleMenu scroll.lua

--- Staiainv13/Graphics/ScreenTitleMenu scroll.lua
+++ Staiainv13/Graphics/ScreenTitleMenu scroll.lua
@@ -4,13 +4,18 @@ return Def.ActorFrame {
 	LoadFont("Common Normal") .. {
 		Text=THEME:GetString("ScreenTitleMenu",gc:GetText()),
 		OnCommand=function(self)
-			self:xy(280,-78):halign(0):valign(0)
+			self:xy(SCREEN_CENTER_X,-80):halign(0):valign(0):halign(0.5):zoom(1.1):shadowlength(5)
 		end,
 		GainFocusCommand=function(self)
-			self:zoom(0.57):diffuse(color("#59307f"))
+			self:bouncebegin(0.1)
+			self:zoom(1.2)
+			self:diffuseshift();
+			self:effectcolor1(color("#7BF77B"));
+			self:effectcolor2(color("#418241"));
+			self:effectperiod(0.5);
 		end,
 		LoseFocusCommand=function(self)
-			self:zoom(0.55):diffuse(color("#b87cf0"))
+			self:stopeffect():bounceend(0.1):zoom(1.1):diffuse(color("#ffffff"));
 		end,
  	}
 }
