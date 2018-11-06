23a8dd4de6ff6da4057f38f7c819c71ca97103f1
Brendon <29536109+11brendon@users.noreply.github.com>
2018-11-06 12:35:09 -0500

    Update MusicWheelItem grades.lua

--- Staiainv13/Graphics/MusicWheelItem grades.lua
+++ Staiainv13/Graphics/MusicWheelItem grades.lua
@@ -14,26 +14,26 @@ return Def.ActorFrame{
 	},
 		Def.Sprite {
 		InitCommand=function(self)
-			self:xy(320,-2):zoomto(4,19)
+			self:xy(310,-2)
 		end,
 		SetGradeCommand=function(self,params)
-			if params.PermaMirror then
-				self:Load(THEME:GetPathG("","mirror"))
-				self:zoomto(24,24)
+			if params.Favorited then
+				self:Load(THEME:GetPathG("","favorite"))
+				self:zoomto(22,26)
 				self:visible(true)
 			else
 				self:visible(false)
 			end
 		end,
 	},
-	Def.Sprite {
+		Def.Sprite {
 		InitCommand=function(self)
-			self:xy(320,-2)
+			self:xy(306,-2):zoomto(4,19)
 		end,
 		SetGradeCommand=function(self,params)
-			if params.Favorited then
-				self:Load(THEME:GetPathG("","favorite"))
-				self:zoomto(16,16)
+			if params.PermaMirror then
+				self:Load(THEME:GetPathG("","mirror"))
+				self:zoomto(20,26)
 				self:visible(true)
 			else
 				self:visible(false)
