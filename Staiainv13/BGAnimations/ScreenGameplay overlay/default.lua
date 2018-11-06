c6cbe5594de74611db04f233d8d5bc348d7c0c0d
Brendon <29536109+11brendon@users.noreply.github.com>
2018-11-06 14:49:27 -0500

    Update default.lua

--- Staiainv13/BGAnimations/ScreenGameplay overlay/default.lua
+++ Staiainv13/BGAnimations/ScreenGameplay overlay/default.lua
@@ -1,5 +1,7 @@
 -- Everything relating to the gameplay screen is gradually moved to WifeJudgmentSpotting.lua
 local t = Def.ActorFrame{}
+t[#t + 1] = LoadActor("spacegorillaiscool")
 t[#t+1] = LoadActor("WifeJudgmentSpotting")
 t[#t+1] = LoadActor("titlesplash")
+t[#t+1] = LoadActor("bn.png") .. { InitCommand=function(self) self:xy(0,0):halign(0):valign(0):diffusealpha(1) end }
 return t
