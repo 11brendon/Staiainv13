21aa5462291f7d1b3dce1c975943abbb0452acf8
Barinade <poco0317@users.noreply.github.com>
2020-06-08 20:00:42 -0500

    fix backgrounds on 16:9

--- BGAnimations/ScreenPlayerOptions underlay.lua
+++ BGAnimations/ScreenPlayerOptions underlay.lua
@@ -13,11 +13,12 @@ if enabled then
 		end
 	}
 end
-t[#t+1] = LoadActor("bgscroll1.png") .. { InitCommand=function(self) self:xy(20,0):halign(0):valign(0):texcoordvelocity(0.002,-0.01):scaletocover(0,0,640,480) end }
-t[#t+1] = LoadActor("bgscroll2.png") .. { InitCommand=function(self) self:xy(0,30):halign(0):valign(0):texcoordvelocity(-0.002,-0.01):scaletocover(0,0,640,480) end }
-t[#t+1] = LoadActor("bgscroll3.png") .. { InitCommand=function(self) self:xy(100,0):halign(0):valign(0):texcoordvelocity(0.002,-0.01):scaletocover(0,0,640,480) end }
-t[#t+1] = LoadActor("bgscroll.png") .. { InitCommand=function(self) self:xy(30,0):halign(0):valign(0):texcoordvelocity(0.002,-0.01):scaletocover(0,0,640,480) end }
-
+t[#t+1] = LoadActor("bn.png") .. { InitCommand=function(self) self:xy(0,-23):halign(0):valign(0):diffusealpha(1):zoomto(SCREEN_WIDTH, SCREEN_HEIGHT) end }
+t[#t+1] = LoadActor("bgscroll1.png") .. { InitCommand=function(self) self:xy(20,0):halign(0):valign(0):texcoordvelocity(0.002,-0.01):scaletocover(0,0,capWideScale(640,1080),capWideScale(480, 1080)) end }
+t[#t+1] = LoadActor("bgscroll2.png") .. { InitCommand=function(self) self:xy(0,30):halign(0):valign(0):texcoordvelocity(-0.002,-0.01):scaletocover(0,0,capWideScale(640,1080),capWideScale(480, 1080)) end }
+t[#t+1] = LoadActor("bgscroll3.png") .. { InitCommand=function(self) self:xy(100,0):halign(0):valign(0):texcoordvelocity(0.002,-0.01):scaletocover(0,0,capWideScale(640,1080),capWideScale(480, 1080)) end }
+t[#t+1] = LoadActor("bgscroll.png") .. { InitCommand=function(self) self:xy(30,0):halign(0):valign(0):texcoordvelocity(0.002,-0.01):scaletocover(0,0,capWideScale(640,1080),capWideScale(480, 1080)) end }
+	
 t[#t+1] = LoadActor(THEME:GetPathG("","_OptionsScreen")) .. {
 		OnCommand=function(self)
 			self:zoomto(SCREEN_WIDTH,SCREEN_HEIGHT):Center():zoom(1):diffusealpha(0)
--- BGAnimations/ScreenTitleMenu underlay.lua
+++ BGAnimations/ScreenTitleMenu underlay.lua
@@ -16,12 +16,13 @@ local t = Def.ActorFrame{
 local frameX = THEME:GetMetric("ScreenTitleMenu","ScrollerX")-10
 local frameY = THEME:GetMetric("ScreenTitleMenu","ScrollerY")
 
-t[#t+1] = LoadActor("bn.png") .. { InitCommand=function(self) self:xy(0,-23):halign(0):valign(0):diffusealpha(1) end }
-t[#t+1] = LoadActor("bgscroll1.png") .. { InitCommand=function(self) self:xy(20,0):halign(0):valign(0):texcoordvelocity(0.002,-0.01):scaletocover(0,0,640,480) end }
-t[#t+1] = LoadActor("bgscroll2.png") .. { InitCommand=function(self) self:xy(0,30):halign(0):valign(0):texcoordvelocity(-0.002,-0.01):scaletocover(0,0,640,480) end }
-t[#t+1] = LoadActor("bgscroll3.png") .. { InitCommand=function(self) self:xy(100,0):halign(0):valign(0):texcoordvelocity(0.002,-0.01):scaletocover(0,0,640,480) end }
-t[#t+1] = LoadActor("bgscroll.png") .. { InitCommand=function(self) self:xy(30,0):halign(0):valign(0):texcoordvelocity(0.002,-0.01):scaletocover(0,0,640,480) end }
 
+t[#t+1] = LoadActor("bn.png") .. { InitCommand=function(self) self:xy(0,-23):halign(0):valign(0):diffusealpha(1):zoomto(SCREEN_WIDTH, SCREEN_HEIGHT) end }
+t[#t+1] = LoadActor("bgscroll1.png") .. { InitCommand=function(self) self:xy(20,0):halign(0):valign(0):texcoordvelocity(0.002,-0.01):scaletocover(0,0,capWideScale(640,1080),capWideScale(480, 1080)) end }
+t[#t+1] = LoadActor("bgscroll2.png") .. { InitCommand=function(self) self:xy(0,30):halign(0):valign(0):texcoordvelocity(-0.002,-0.01):scaletocover(0,0,capWideScale(640,1080),capWideScale(480, 1080)) end }
+t[#t+1] = LoadActor("bgscroll3.png") .. { InitCommand=function(self) self:xy(100,0):halign(0):valign(0):texcoordvelocity(0.002,-0.01):scaletocover(0,0,capWideScale(640,1080),capWideScale(480, 1080)) end }
+t[#t+1] = LoadActor("bgscroll.png") .. { InitCommand=function(self) self:xy(30,0):halign(0):valign(0):texcoordvelocity(0.002,-0.01):scaletocover(0,0,capWideScale(640,1080),capWideScale(480, 1080)) end }
+	
 t[#t+1] = LoadFont("Common Formal") .. {
 	InitCommand=function(self)
 		self:xy(2,478):zoom(0.55):valign(1):halign(0)
